e4c1fbeab01aa339c575712530eb10bfb4fee22b
helloWorld-notFound-404 <hello.world66134@gmail.com>
2024-11-07 18:42:09 +0600

    Delete js/variable.js

--- js/variable.js
+++
@@ -1,8 +0,0 @@
-console.log("hello world!");
-
-const pi = 3.1416;
-let fullname = "hello world";
-var age = 19; // prefer not to use var because of issue of block scope and functional scope
-let number;
-
-console.table([pi, fullname, age, number]);
