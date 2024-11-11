1a7e9f1ed9af3f49051d328290f47f54b7a09fed
helloWorld-notFound-404 <hello.world66134@gmail.com>
2024-11-11 02:01:56 +0000

    config js

--- js/08_objects.js
+++ js/08_objects.js
@@ -34,11 +34,61 @@ console.log(user);
 user.greeting = function(){
     console.log("Hello world");
 }
-console.log(user.greeting); // [Function (anoynymous)]
+console.log(user.greeting); // [Function (anoynymous)]; function reference
 console.log(user.greeting());  // Hello world
 
 user.greeting2 = function(){
-    console.log(`Hello world, ${this.name}`);
+    console.log(`Hello world, ${this.name}`); // Hello world, Proshanta
 }
 console.log(user.greeting2());
 
+const tinderUser = new Object(); // same as const tinderUser = {}; singletone object
+console.log(tinderUser); // {}
+tinderUser.id = '123abc';
+tinderUser.name = 'Hello';
+tinderUser.isLogIn = false;
+console.log(tinderUser);
+
+const regularUser = {
+    email: 'hello@gmail.com',
+    fullname: {
+        username: {
+            firstname: 'Proshanta',
+            lastname: 'Pal'
+        }
+    }
+}
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.username.firstname);
+
+const obj1 = {1: 'a', 2: 'b'}
+const obj2 = {3: 'c', 4: 'd'}
+
+const obj3 = {obj1, obj2};
+console.log(obj3);
+const obj4 = Object.assign({}, obj1, obj2); // Object.assign(target, source)
+console.log(obj4); 
+const obj5 = {...obj1, ...obj2}; // using spread
+console.log(obj5);
+
+const users = [
+    {
+        id: 1,
+        roll: 34
+    },
+    {
+        id: 2,
+        roll: 67
+    },
+    {
+        id: 3,
+        roll: 90
+    }
+]
+console.log(users[1].id); // 2
+
+console.log(tinderUser); // { id: '123abc', name: 'Hello', isLogIn: false }
+console.log(Object.keys(tinderUser)); // returns an array; // [ 'id', 'name', 'isLogIn' ]
+console.log(Object.values(tinderUser)); // returns an array; // [ '123abc', 'Hello', false ]
+console.log(Object.entries(tinderUser)); // returns an array; // [ [ 'id', '123abc' ], [ 'name', 'Hello' ], [ 'isLogIn', false ] ]
+console.log(tinderUser.hasOwnProperty('isLogIn')); // true
